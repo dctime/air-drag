52cdde189a65c9df62f616939c82101aedec9ad3
DCtime <48884643+DCtime@users.noreply.github.com>
2021-01-03 00:43:51 +0800

    No drag bug

--- 空氣阻力實驗.py
+++ 空氣阻力實驗.py
@@ -8,23 +8,24 @@ all_speed_2 = []
 #F=(1/2)CρSV^2
 #x = vot + 1/2at^2
 
-min_time = 0.01
+min_time = 0.1
+data_count = 100
 
 #初始化
 speed = 0
 x = 0
 #可調整
 mass = 10
-affected_area = 100
+affected_area = 1
 
 
 
 def drag_force(current_speed, affected_area, drag_coefficient = 0.4, air_density = 1.225):
-    #print("drag_force:", drag_coefficient * air_density * affected_area * current_speed * current_speed / 2)
+    print("drag_force:", drag_coefficient * air_density * affected_area * current_speed * current_speed / 2)
     return drag_coefficient * air_density * affected_area * current_speed * current_speed / 2
 
 def gravity_force(mass, gravity = 9.8):
-    #print("gravity_force:", mass * gravity)
+    print("gravity_force:", mass * gravity)
     return mass * gravity
 
 def final_acceleration(gravity_force, drag_force, mass):
@@ -39,7 +40,7 @@ def delta_time_speed(acceleration, min_time, current_speed):
     #print("delta_time_speed:", current_speed + acceleration * min_time)
     return current_speed + acceleration * min_time
 
-for i in range(100):
+for i in range(data_count):
     x += delta_time_move(final_acceleration(gravity_force(mass), drag_force(speed, affected_area), mass), min_time, speed)
     speed += delta_time_speed(final_acceleration(gravity_force(mass), drag_force(speed, affected_area), mass), min_time, speed)
     #print()
@@ -59,10 +60,10 @@ speed2 = 0
 x2 = 0
 #可調整
 mass2 = 10
-affected_area2 = 200
+affected_area2 = 100
 
 
-for i in range(100):
+for i in range(data_count):
     x2 += delta_time_move(final_acceleration(gravity_force(mass2), drag_force(speed2, affected_area2), mass2), min_time, speed2)
     speed2 += delta_time_speed(final_acceleration(gravity_force(mass2), drag_force(speed2, affected_area2), mass2), min_time, speed2)
     #print()
@@ -74,14 +75,22 @@ for i in range(100):
     all_position_2.append(x2)
     all_speed_2.append(speed2)
 
-plt.subplot(2, 1, 1)
-plt.plot(all_position)
-plt.plot(all_position_2)
+plt.subplot(1, 2, 1)
+posLine1 = plt.plot(all_position, label="S = " + str(affected_area))
+posLine2 = plt.plot(all_position_2, label="S = " + str(affected_area2))
+plt.legend(loc='best')
+plt.title("x-t graph", fontsize = 24)
+plt.xlabel("Time (1 = " + str(min_time) + "sec)", fontsize = 16)
+plt.ylabel("x", fontsize = 16)
 plt.tick_params(axis='both', labelsize = 12, color='red')
 
-plt.subplot(2, 1, 2)
-plt.plot(all_speed)
-plt.plot(all_speed_2)
+plt.subplot(1, 2, 2)
+plt.title("v-t graph", fontsize = 24)
+plt.xlabel("Time (1 = " + str(min_time) + "sec)", fontsize = 16)
+plt.ylabel("v", fontsize = 16)
+posLine1 = plt.plot(all_speed, label="S = " + str(affected_area))
+posLine2 = plt.plot(all_speed_2, label="S = " + str(affected_area2))
+plt.legend(loc='best')
 plt.tick_params(axis='both', labelsize = 12, color='red')
 
 plt.show()
