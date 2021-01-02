61b94f14febca65d58ba7fd47214f0119ec3a305
DCtime <48884643+DCtime@users.noreply.github.com>
2021-01-03 00:07:07 +0800

    Showing x-t and v-t

--- 空氣阻力實驗.py
+++ 空氣阻力實驗.py
@@ -1,15 +1,23 @@
-speed = 0
-mass = 10
-affected_area = 10
-x = 0
-min_time = 0.01
+import matplotlib.pyplot as plt
 
 all_position = []
 all_speed = []
 
-
+all_position_2 = []
+all_speed_2 = []
 #F=(1/2)CρSV^2
 #x = vot + 1/2at^2
+
+min_time = 0.01
+
+#初始化
+speed = 0
+x = 0
+#可調整
+mass = 10
+affected_area = 100
+
+
 
 def drag_force(current_speed, affected_area, drag_coefficient = 0.4, air_density = 1.225):
     #print("drag_force:", drag_coefficient * air_density * affected_area * current_speed * current_speed / 2)
@@ -31,7 +39,7 @@ def delta_time_speed(acceleration, min_time, current_speed):
     #print("delta_time_speed:", current_speed + acceleration * min_time)
     return current_speed + acceleration * min_time
 
-for i in range(500):
+for i in range(100):
     x += delta_time_move(final_acceleration(gravity_force(mass), drag_force(speed, affected_area), mass), min_time, speed)
     speed += delta_time_speed(final_acceleration(gravity_force(mass), drag_force(speed, affected_area), mass), min_time, speed)
     #print()
@@ -41,7 +49,41 @@ for i in range(500):
     #print("--------------------------------------------")
     #print()
     all_position.append(x)
-    all_position.append(speed)
+    all_speed.append(speed)
 
-print(all_position)
-print(all_speed)
+#print(all_position)
+#print(all_speed)
+
+#初始化
+speed2 = 0
+x2 = 0
+#可調整
+mass2 = 10
+affected_area2 = 200
+
+
+for i in range(100):
+    x2 += delta_time_move(final_acceleration(gravity_force(mass2), drag_force(speed2, affected_area2), mass2), min_time, speed2)
+    speed2 += delta_time_speed(final_acceleration(gravity_force(mass2), drag_force(speed2, affected_area2), mass2), min_time, speed2)
+    #print()
+    #print("--------------------------------------------")
+    #print(">>> now position <<<", x2)
+    #print(">>> now speed <<<", speed2)
+    #print("--------------------------------------------")
+    #print()
+    all_position_2.append(x2)
+    all_speed_2.append(speed2)
+
+plt.subplot(2, 1, 1)
+plt.plot(all_position)
+plt.plot(all_position_2)
+plt.tick_params(axis='both', labelsize = 12, color='red')
+
+plt.subplot(2, 1, 2)
+plt.plot(all_speed)
+plt.plot(all_speed_2)
+plt.tick_params(axis='both', labelsize = 12, color='red')
+
+plt.show()
+
+
